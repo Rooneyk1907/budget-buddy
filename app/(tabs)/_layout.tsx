65a4b3423329964b4b2ac991bff9757a28a915f5
Kevin Rooney <rooneyjkevin@proton.megit config --global user.name Rooneyk1907>
2026-04-07 17:06:41 -0500

    root layout and tab layout created

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,35 +1,51 @@
+import Ionicons from '@expo/vector-icons/Ionicons';
 import { Tabs } from 'expo-router';
-import React from 'react';
-
-import { HapticTab } from '@/components/haptic-tab';
-import { IconSymbol } from '@/components/ui/icon-symbol';
-import { Colors } from '@/constants/theme';
-import { useColorScheme } from '@/hooks/use-color-scheme';
+import { Pressable, Text, View } from 'react-native';
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
+	return (
+		<Tabs>
+			<Tabs.Screen
+				name='index'
+				options={{
+					title: 'Dashboard',
+					tabBarButton: (props) => (
+						<TabButton
+							{...props}
+							label='Dashboard'
+							icon='stats-chart'
+							iconOutline='stats-chart-outline'
+						/>
+					),
+				}}
+			/>
+		</Tabs>
+	);
+}
 
-  return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
-    </Tabs>
-  );
+function TabButton({
+	accessibilityState,
+	onPress,
+	label,
+	icon,
+	iconOutline,
+	...rest
+}: any) {
+	const focused = accessibilityState?.selected;
+
+	return (
+		<Pressable
+			onPress={onPress}
+			{...rest}>
+			<View>
+				<Ionicons
+					name={focused ? icon : iconOutline}
+					size={20}
+					coloor={focused ? '#fff' : '#bbb'}
+					style={{ marginBottom: 4 }}
+				/>
+				<Text>{label}</Text>
+			</View>
+		</Pressable>
+	);
 }
